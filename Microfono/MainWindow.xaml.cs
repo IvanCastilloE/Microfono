b539ba25d0214fb568f3ccf426e3fdf6d4646c5f
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-31 11:48:07 -0700

    Agregar archivos de proyecto.

---
+++ Microfono/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NAudio;
+using NAudio.Wave;
+using NAudio.Dsp;
+
+namespace Microfono
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WaveIn waveIn; //Conecta con el microfono
+        WaveFormat formato; //formato de audio
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+        }
+
+        private void BtnIniciar_Click(object sender, RoutedEventArgs e)
+        {
+            //Inicializar la conexion
+            waveIn = new WaveIn();
+            //Establecer el formato (F. muestreo, profundidad de bits, No. canales)
+            waveIn.WaveFormat = new WaveFormat(44100, 16, 1);
+            formato = waveIn.WaveFormat;
+
+            //Duracion del buffer
+            waveIn.BufferMilliseconds = 500;
+
+            //Con que funcion respondemos cuando se llena el buffer
+            waveIn.DataAvailable += WaveIn_DataAvailable;
+
+            waveIn.StartRecording();
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
